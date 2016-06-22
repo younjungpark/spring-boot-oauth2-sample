9bcfc15f36fa70624f1e66c9e15ba91f3a354384
YounJung Park <handsomeboy72@gmail.com>
2016-06-22 18:21:05 +0900

    added @EnableWebSecurity annotation

--- api-server/src/main/java/com/example/ApiApplication.java
+++ api-server/src/main/java/com/example/ApiApplication.java
@@ -5,15 +5,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
-import org.springframework.security.oauth2.provider.token.TokenStore;
-import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
-
-import javax.sql.DataSource;
 
 @EnableResourceServer
 @SpringBootApplication
+@EnableWebSecurity
 public class ApiApplication {
 
 	@Bean
--- oauth2-server/src/main/java/com/example/OAuth2Application.java
+++ oauth2-server/src/main/java/com/example/OAuth2Application.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.oauth2.authserver.OAuth2AuthorizationServerConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.provider.token.TokenStore;
@@ -12,6 +13,7 @@ import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
 @EnableAuthorizationServer
 @SpringBootApplication
+@EnableWebSecurity
 public class OAuth2Application extends OAuth2AuthorizationServerConfiguration {
 	@Override
 	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
