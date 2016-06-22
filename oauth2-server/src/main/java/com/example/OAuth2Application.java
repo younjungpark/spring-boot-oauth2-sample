5ca39f578ffe47026e58374b9bc965d3a51b144c
YounJung Park <handsomeboy72@gmail.com>
2016-06-22 16:11:54 +0900

    apply JWT

--- oauth2-server/src/main/java/com/example/OAuth2Application.java
+++ oauth2-server/src/main/java/com/example/OAuth2Application.java
@@ -2,19 +2,32 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.authserver.OAuth2AuthorizationServerConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.provider.token.TokenStore;
-import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
-
-import javax.sql.DataSource;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
 @EnableAuthorizationServer
 @SpringBootApplication
-public class OAuth2Application {
+public class OAuth2Application extends OAuth2AuthorizationServerConfiguration {
+	@Override
+	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+		super.configure(endpoints);
+		endpoints.accessTokenConverter(jwtAccessTokenConverter());
+	}
+
 	@Bean
-	public TokenStore jdbcTokenStore(DataSource dataSource) {
-		return new JdbcTokenStore(dataSource);
+	public TokenStore tokenStore() {
+		return new JwtTokenStore(jwtAccessTokenConverter());
+	}
+
+	@Bean
+	public JwtAccessTokenConverter jwtAccessTokenConverter()
+	{
+		return new JwtAccessTokenConverter();
 	}
 
 	public static void main(String[] args) {
